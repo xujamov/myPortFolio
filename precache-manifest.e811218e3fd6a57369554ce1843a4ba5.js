1130d7168ab555cd41c6854ec97fb76bc4f9db2a
xujamov <doniyorprince95@gmail.com>
2021-01-06 10:22:42 +0000

    Deploying to gh-pages from  @ d47a24d8e91699cf1d20f18f52bf84b7e5e2de47 🚀

---
+++ precache-manifest.e811218e3fd6a57369554ce1843a4ba5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0af83f49b19041f0cacdd67f8a129a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c50e8af0b8b2fed90f9",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "90609c02b0896f61079d",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "3061cf5bc03ed40461dd",
+    "url": "/static/css/main.91308f57.chunk.css"
+  },
+  {
+    "revision": "be6a1c1c3cf81bede313",
+    "url": "/static/js/2.06a5f97f.chunk.js"
+  },
+  {
+    "revision": "3c50e8af0b8b2fed90f9",
+    "url": "/static/js/3.1f299f89.chunk.js"
+  },
+  {
+    "revision": "90609c02b0896f61079d",
+    "url": "/static/js/4.d2b2d62f.chunk.js"
+  },
+  {
+    "revision": "3061cf5bc03ed40461dd",
+    "url": "/static/js/main.3f14ea10.chunk.js"
+  },
+  {
+    "revision": "f37f225731eb64810d64",
+    "url": "/static/js/runtime-main.3388f33d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "a247416ea3311e9b34375d82d8d69fa9",
+    "url": "/static/media/DeshonQala.a247416e.JPG"
+  },
+  {
+    "revision": "0bd8d50d48c824a4c89f81ab47fbb94b",
+    "url": "/static/media/IchanQala.0bd8d50d.JPG"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5b50167135a57b7226b892de318c90e4",
+    "url": "/static/media/OquvQollanma.5b501671.JPG"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "26bc4be4791ddf86e56c940ac61f518d",
+    "url": "/static/media/doniyor_main.26bc4be4.jpg"
+  },
+  {
+    "revision": "5b58551b59cdf1d95ee107452488e2e3",
+    "url": "/static/media/doniyor_working.5b58551b.jpg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
